304f2f6ecc6de1fe45dfad43b7998f7122a06b62
kimberlytumober <kimberly.tumober5@gmail.com>
2025-11-21 20:12:32 +0800

    Update aplikasi.manajemen.kontak.sederhana.py

--- aplikasi.manajemen.kontak.sederhana.py
+++ aplikasi.manajemen.kontak.sederhana.py
@@ -1,1 +1,11 @@
+# Aplikasi Manajemen Kontak Sederhana
 
+kontak = {}
+
+def tampilkan_kontak():
+    if not kontak:
+        print("\nBelum ada kontak yang tersimpan.")
+    else:
+        print("\nDaftar Kontak:")
+        for nama, nomor in kontak.items():
+            print(f"- {nama} : {nomor}")
